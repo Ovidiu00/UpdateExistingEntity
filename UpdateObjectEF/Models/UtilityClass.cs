22f1a6a767e4d6ef5d9169c32bfba8fcfd7ae22b
Ovidiu00 <63671460+Ovidiu00@users.noreply.github.com>
2021-10-10 00:08:14 +0300

    Update UtilityClass.cs

--- UpdateObjectEF/Models/UtilityClass.cs
+++ UpdateObjectEF/Models/UtilityClass.cs
@@ -16,16 +16,16 @@ namespace UpdateObjectEF.Models
             this.context = context;
         }
 
-        public  void UpdateIfModified<T>(T existingEntity, T modifiedExistingEntity)
-        {
         /// <summary>
         /// 
         /// Modified existing entity's proprieites and changes are reflected when SaveChanges is called.Long story short : Overrides the existing entity with the new entity given as parameter.
         /// Only applies to Value type and string proprieties.
-            
+        
         /// </summary>
         /// <param name="existingEntity">Entity that was retrieved from DB (actual values live in DB).If it is not tracked by the context already, it will be tracked after calling this method</param>
         /// <param name="modifiedExistingEntity">Entity recieved in http PUT endpoint or which the client knows exist in DB</param>
+        public  void UpdateIfModified<T>(T existingEntity, T modifiedExistingEntity)
+        {
             foreach (var property in existingEntity.GetType().GetProperties())
             {
                 object valueExistingEmployee = typeof(T).GetProperty(property.Name).GetValue(existingEntity);
